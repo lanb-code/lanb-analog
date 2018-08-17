b0e9bc19ddae98ee88a2b3ad00a12779493340ad
colodoo <lodog1@qq.com>
2018-08-17 23:55:49 +0800

    简化取参数代码

--- src/lanb-analog.js
+++ src/lanb-analog.js
@@ -1,59 +1,66 @@
 const ah = require('ajax-hook')
+require('url-search-params-polyfill')
 
-module.exports = (() => {
-    // get URL value
-    function get(search, name) {
-        var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i")
-        var r = search.substr(1).match(reg)
-        if (r != null)
-            return (r[2])
-        return null
+module.exports = ((window) => {
+
+  // check browser environment
+  if (typeof window === 'undefined') {
+    console.error('lanb-analog.js: Make sure you are in browser environment.')
+    return;
+  }
+
+  // get URL value
+  function get (search, name) {
+    let params = new URLSearchParams(search)
+    if (params.has(name)) return params.get(name)
+    else return null
+  }
+
+  // match analog data
+  function matchData (analog, xhr) {
+    let search = '?' + xhr.responseURL.split('?')[1]
+    let url = get(search, 'url')
+    for (let i = 0; i < analog.data.length; i++) {
+      if (analog.data[i].url === url) {
+        xhr.responseText = JSON.stringify(analog.data[i].data)
+      }
     }
+  }
 
+  // default alalog option
+  let defaultOption = {
+    // hook callbacks
+    onreadystatechange: (xhr) => {
+      console.log("onreadystatechange called: %O", xhr)
+    },
+    onload: (xhr) => {
+      if (typeof analog === 'undefined') {
+        let analog = {}
+        analog.data = []
+        matchData(analog, xhr)
+      } else {
+        matchData(analog, xhr)
+      }
 
-    function matchdata(analog, xhr) {
-        let search = '?' + xhr.responseURL.split('?')[1]
-        let url = get(search, 'url')
-        for (let i = 0; i < analog.data.length; i++) {
-            if (analog.data[i].url == url) {
-                xhr.responseText = JSON.stringify(analog.data[i].data)
-            }
-        }
+    },
+    // hook function
+    open: (arg, xhr) => {
+      console.log("open called: %O", xhr)
+    },
+    onerror: (xhr) => {
+      console.log('onerror called: %O', xhr)
     }
+  }
 
-    // default alalog option
-    let defaultOption = {
-        // hook callbacks
-        onreadystatechange: (xhr) => {
-            console.log("onreadystatechange called: %O", xhr)
-        },
-        onload: (xhr) => {
-            if (typeof analog == 'undefined') {
-                let analog = {}
-                analog.data = []
-                matchdata(analog, xhr)
-            } else {
-                matchdata(analog, xhr)
-            }
+  ah.hookAjax(defaultOption)
 
-        },
-        // hook function
-        open: (arg, xhr) => {
-            console.log("open called: %O", xhr)
-        },
-        onerror: (xhr) => {
-            console.log('onerror called: %O', xhr)
-        }
-    }
+  // exposed object
+  window.Analog = ah
 
-    ah.hookAjax(defaultOption)
+  // extension method
+  Analog.fn = (option) => {
+    let newOption = Object.assign(defaultOption, option)
+    Analog.hookAjax(newOption)
+  }
 
-    // exposed object
-    window.Analog = ah
-
-    // extension method
-    Analog.fn = (option) => {
-        let newOption = Object.assign(defaultOption, option)
-        Analog.hookAjax(newOption)
-    }
-})()
+})(window)
