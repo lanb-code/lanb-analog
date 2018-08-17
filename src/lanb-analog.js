ad1dcd043f5808ffba4f58cea9e9c51a91036424
colodoo <lodog1@qq.com>
2018-08-17 15:49:31 +0800

    解决analog未定义问题

--- src/lanb-analog.js
+++ src/lanb-analog.js
@@ -1,6 +1,7 @@
 const ah = require('ajax-hook')
 
 module.exports = (() => {
+    // get URL value
     function get(search, name) {
         var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i")
         var r = search.substr(1).match(reg)
@@ -9,21 +10,34 @@ module.exports = (() => {
         return null
     }
 
+
+    function matchdata(analog, xhr) {
+        let search = '?' + xhr.responseURL.split('?')[1]
+        let url = get(search, 'url')
+        for (let i = 0; i < analog.data.length; i++) {
+            if (analog.data[i].url == url) {
+                xhr.responseText = JSON.stringify(analog.data[i].data)
+            }
+        }
+    }
+
+    // default alalog option
     let defaultOption = {
-        //hook callbacks
+        // hook callbacks
         onreadystatechange: (xhr) => {
             console.log("onreadystatechange called: %O", xhr)
         },
         onload: (xhr) => {
-            var search = '?' + xhr.responseURL.split('?')[1];
-            var url = get(search, 'url');
-            for (let i = 0; i < analog.data.length; i++) {
-                if (analog.data[i].url == url) {
-                    xhr.responseText = JSON.stringify(analog.data[i].data)
-                }
+            if (typeof analog == 'undefined') {
+                let analog = {}
+                analog.data = []
+                matchdata(analog, xhr)
+            } else {
+                matchdata(analog, xhr)
             }
+
         },
-        //hook function
+        // hook function
         open: (arg, xhr) => {
             console.log("open called: %O", xhr)
         },
@@ -33,8 +47,11 @@ module.exports = (() => {
     }
 
     ah.hookAjax(defaultOption)
+
+    // exposed object
     window.Analog = ah
 
+    // extension method
     Analog.fn = (option) => {
         let newOption = Object.assign(defaultOption, option)
         Analog.hookAjax(newOption)
